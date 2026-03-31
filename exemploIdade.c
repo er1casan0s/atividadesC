4ffadfbe4e6959e32d3a9fcb973376acd1bb119c
Erica Santos <erica.santos.lima15@gmail.com>
2026-03-31 16:07:23 -0300

    Add idade comparison in exemploIdade.c

---
+++ exemploIdade.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int idade1 = 50;
+  int idade2 = 25;
+  
+  if(idade1 !=idade2){
+      printf("As idades são diferentes.\n");
+  }else{
+      printf("As idades são iguais.\n");
+  }
+  
+  
+  
+    return 0;
+}
