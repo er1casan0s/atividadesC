cbcdbeef4db9b90bc329000e1e07a78cef303ea7
Erica Santos <erica.santos.lima15@gmail.com>
2026-03-31 15:23:19 -0300

    Add example of if-else statement for grading

---
+++ exemploNotaIFELSE.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float nota = 3.0;
+    
+    if(nota >= 7.0){
+        printf("Parabéns, você passou!");
+    }else{
+        printf("Infelizmente, você não passou!");
+    }
+    
+    return 0;
+}
